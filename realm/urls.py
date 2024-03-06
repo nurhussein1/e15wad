1126347e9baffda427ea3259c8a22eb282a6a9c1
Adam Tong <2733692t@student.gla.ac.uk>
2024-03-06 22:53:21 +0000

    Most pages have been intialised, just the individual book pages needed

--- realm/urls.py
+++ realm/urls.py
@@ -8,5 +8,16 @@ urlpatterns = [
  path('', views.home, name='Home'),
  path('about/', views.about, name = 'About'),
  path('register/',views.register,name = 'Register'),
-
+ path('login/',views.login,name = 'Login'),
+ path('categories/',views.categories,name = 'Categories'),
+ path('popularbooks/',views.popularbooks,name = 'PopularBooks'),
+ path('account/',views.account,name = 'Account'),
+ path('account/profilepicture/',views.profilepicture,name = 'ProfilePictures'),
+ path('account/myreviews/',views.myreviews,name = 'MyReviews'),
+ path('account/mybooks/',views.mybooks,name = 'MyBooks'),
+ path('categories/historical/',views.historical,name = 'Historical'),
+ path('categories/scifi/',views.scifi,name = 'SciFi'),
+ path('categories/classics/',views.classics,name = 'Classics'),
+ path('categories/thriller/',views.thriller,name = 'Thiller'),
+ path('categories/fantasy/',views.fantasy,name = 'Fantasy'),
 ]
--- realm/views.py
+++ realm/views.py
@@ -20,3 +20,63 @@ def register(request):
  
     context_dict = {'boldmessage': 'this is the register page, test that context_dict works'}
     return render(request, 'realm/register.html', context=context_dict)
+
+def login(request):
+ 
+    context_dict = {'boldmessage': 'this is the login page, test that context_dict works'}
+    return render(request, 'realm/login.html', context=context_dict)
+
+def categories(request):
+ 
+    context_dict = {'boldmessage': 'this is the categories page, test that context_dict works'}
+    return render(request, 'realm/categories.html', context=context_dict)
+
+def popularbooks(request):
+ 
+    context_dict = {'boldmessage': 'this is the popular books page, test that context_dict works'}
+    return render(request, 'realm/popularbooks.html', context=context_dict)
+
+def account(request):
+ 
+    context_dict = {'boldmessage': 'this is the account page, test that context_dict works'}
+    return render(request, 'realm/account.html', context=context_dict)
+
+def profilepicture(request):
+ 
+    context_dict = {'boldmessage': 'this is the profile picture page, test that context_dict works'}
+    return render(request, 'realm/account/profilepicture.html', context=context_dict)
+
+def myreviews(request):
+ 
+    context_dict = {'boldmessage': 'this is the my reviews page, test that context_dict works'}
+    return render(request, 'realm/account/myreviews.html', context=context_dict)
+
+def mybooks(request):
+ 
+    context_dict = {'boldmessage': 'this is the my books page, test that context_dict works'}
+    return render(request, 'realm/account/mybooks.html', context=context_dict)
+
+def historical(request):
+ 
+    context_dict = {'boldmessage': 'this is the historicals page, test that context_dict works'}
+    return render(request, 'realm/categories/historical.html', context=context_dict)
+
+def scifi(request):
+ 
+    context_dict = {'boldmessage': 'this is the science fiction page, test that context_dict works'}
+    return render(request, 'realm/categories/scifi.html', context=context_dict)
+
+def classics(request):
+ 
+    context_dict = {'boldmessage': 'this is the classics page, test that context_dict works'}
+    return render(request, 'realm/categories/classics.html', context=context_dict)
+
+def thriller(request):
+ 
+    context_dict = {'boldmessage': 'this is the thrillers page, test that context_dict works'}
+    return render(request, 'realm/categories/thriller.html', context=context_dict)
+
+def fantasy(request):
+ 
+    context_dict = {'boldmessage': 'this is the fantasy page, test that context_dict works'}
+    return render(request, 'realm/categories/fantasy.html', context=context_dict)
