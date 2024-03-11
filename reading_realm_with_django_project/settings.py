1f2193adfe5ed29981dc63636524a7f4fa6c49f5
Adam Tong <2733692t@student.gla.ac.uk>
2024-03-11 22:12:33 +0000

    Updated main with home.html and myreviews.html

--- reading_realm_with_django_project/settings.py
+++ reading_realm_with_django_project/settings.py
@@ -13,7 +13,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -126,3 +126,8 @@ LOGIN_URL = 'auth_login'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
